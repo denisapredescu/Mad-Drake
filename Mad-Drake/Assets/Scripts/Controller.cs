9f568d6810e0bd3ba679cb453b0dc64f3333df2c
Rares Popa <rares_teodor@yahoo.ro>
2022-10-26 11:59:14 +0300

    Character controller 1.0

---
+++ Mad-Drake/Assets/Scripts/Controller.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private new Transform transform;
+    private Rigidbody2D rb2;
+    private TrailRenderer trailRenderer;
+
+    private float xMovement = 0.0f, yMovement = 0.0f;
+    public float speed = 6.0f;
+    public float dashForce = 30.0f;
+    [Range(0.01f, 0.99f)]
+    public float changeDashForce = 0.9f;
+    public float breakDash = 4.0f;
+    private bool dashing = false;
+    private bool startDash = false;
+
+    private void Start()
+    {
+        transform = this.gameObject.transform;
+        rb2 = this.gameObject.GetComponent<Rigidbody2D>();
+        trailRenderer = this.gameObject.GetComponent<TrailRenderer>();
+    }
+
+    private void Update() 
+    {
+        xMovement = Input.GetAxis("Horizontal");
+        yMovement = Input.GetAxis("Vertical");
+
+        if (!dashing)
+        {
+            //moving
+            transform.Translate(new Vector3(xMovement, yMovement, 0) * Time.deltaTime * speed);
+
+            //rotate left and right
+            if (xMovement > 0.0f && transform.localScale.x < 0.0f)
+            {
+                transform.localScale = new Vector3(
+                    -transform.localScale.x,
+                    transform.localScale.y,
+                    transform.localScale.z);
+            }
+            else if (xMovement < 0.0f && transform.localScale.x > 0.0f)
+            {
+                transform.localScale = new Vector3(
+                    -transform.localScale.x,
+                    transform.localScale.y,
+                    transform.localScale.z);
+            }
+            
+            //dashing
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                startDash = true;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //start dashing
+        if(startDash && rb2 != null)
+        {
+            rb2.velocity = new Vector2(xMovement, yMovement).normalized * dashForce;
+            trailRenderer.emitting = true;
+            startDash = false;
+            dashing = true;
+        }
+
+        //handle dash logic
+        if(dashing && rb2.velocity.magnitude < breakDash)
+        {
+            dashing = false;
+            rb2.velocity = Vector2.zero;
+            trailRenderer.emitting = false;
+        }
+        else if(dashing)
+        {
+            rb2.velocity = rb2.velocity * changeDashForce;
+        }
+    }
+}
