29c98b61ba34d45c09693aae5af8f338113f886a
Marian Neagu <marianmadalinneagu@gmail.com>
2022-11-06 21:31:03 +0200

    added PlayerHealthController
    Signed-off-by: Marian Neagu <marianmadalinneagu@gmail.com>

--- Mad-Drake/Assets/Scripts/HealthBar.cs
+++ Mad-Drake/Assets/Scripts/HealthBar.cs
@@ -8,6 +8,7 @@ public class HealthBar : MonoBehaviour
     public Slider slider;
     public Gradient gradient;
     public Image fill;
+
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
