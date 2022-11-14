560c59217d536037afe0207457c4174018849946
Rares Popa <rares_teodor@yahoo.ro>
2022-11-14 20:46:34 +0200

    Gun effects

--- Mad-Drake/Assets/Scripts/BulletMovement.cs
+++ Mad-Drake/Assets/Scripts/BulletMovement.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class BulletMovement : MonoBehaviour
 {
     private new Transform transform = null;
+    private SpriteRenderer spriteRenderer = null;
     [SerializeField]
     private float speed = 0.0f;
     [SerializeField]
     private float timeUntilDestruction = 5.0f;
+    //a function that adds this bullet refernce in the queue of a controller for later use
     private Action<GameObject> addBullet = null;
     private bool startDeactivating = false;
+    [SerializeField]
+    private ParticleSystem explosionEffect;
 
     private void Start()
     {
         transform = GetComponent<Transform>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //auto disable bullet if it goes too far
         StartCoroutine(WaitToDeactivate());
     }
 
@@ -25,31 +31,43 @@ public class BulletMovement : MonoBehaviour
 
     private void Update()
     {
+        //used when the raycast hits a collider
         if(startDeactivating)
         {
-            Deactivate();
+            StartCoroutine(Deactivate());
             return;
         }
+        else
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
 
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        //moving the bullet with translate for performance
     }
 
     private void FixedUpdate()
     {
+        //check if the bullet would hit something, it is needed because using translate can lead to the bullet teleporting without touching the collider
         if(Physics2D.Raycast(transform.position, transform.TransformDirection(Vector3.right), speed * Time.fixedDeltaTime - 0.2f))
         {
             startDeactivating = true;
         }
     }
 
+    //passing the function from the controller back to the bullet to use it
     public void setActionAddBullet(Action<GameObject> action)
     {
         addBullet = action;
     }
 
-    private void Deactivate()
+    private IEnumerator Deactivate()
     {
+        explosionEffect.Play();
+        spriteRenderer.enabled = false;
+        //waits for the simulation to play
+        yield return new WaitForSeconds(explosionEffect.main.duration / explosionEffect.main.simulationSpeed);
         gameObject.SetActive(false);
+        spriteRenderer.enabled = true;
         startDeactivating = false;
         addBullet(gameObject);
     }
--- Mad-Drake/Assets/Scripts/GunFiringController.cs
+++ Mad-Drake/Assets/Scripts/GunFiringController.cs
@@ -7,17 +7,26 @@ public class GunFiringController : MonoBehaviour
     [SerializeField]
     private GameObject bullet = null;
     [SerializeField]
-    private Transform tipOfGun = null;
+    private GameObject tipOfGun = null;
+    private Transform tipTransform = null;
+    //a list of all particle effects that need to start when the gun fires
+    private ParticleSystem[] tipParticleEffects = null;
     [SerializeField]
     private Transform parent = null;
     [SerializeField]
     private float firingDelay = 1.0f;
     private bool canFire = true;
     private Quaternion rotate180 = Quaternion.Euler(0, 0, 180);
-
+    [SerializeField]
+    private int magazineSize = 10;
+    private int activeMagazine = 0;
+    [SerializeField]
+    private float reloadTime = 2.0f;
+    //references to reuse the bullets
     private Queue<GameObject> activeBullets;
     private GameObject activeBullet = null;
 
+    //the function passed to a bullet to add itself to a queue
     private void addBullet(GameObject gameObject)
     {
         this.activeBullets.Enqueue(gameObject);
@@ -26,25 +35,47 @@ public class GunFiringController : MonoBehaviour
     private void Start()
     {
         activeBullets = new Queue<GameObject>();
+        tipTransform = tipOfGun.transform;
+        activeMagazine = magazineSize;
+
+        //getting all the particle effects and making them 10 times faster
+        tipParticleEffects = tipOfGun.GetComponentsInChildren<ParticleSystem>();
     }
 
     void Update()
     {
-        if(canFire && Input.GetMouseButton(0))
+        //reload with delay
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+        }
+
+        //fire rate
+        if (canFire && activeMagazine > 0 && Input.GetMouseButton(0))
         {
             StartCoroutine(waitToFire());
-            if(parent.localScale.x < 0.0f)
+            activeMagazine--;
+
+            //muzzle flash for the gun
+            foreach (var ParticleEffect in tipParticleEffects)
             {
-                if(activeBullets.Count > 0)
+                ParticleEffect.Play();
+            }
+
+            //if x-scale is -1 there is applied a 180 rotation to the bullet
+            if (parent.localScale.x < 0.0f)
+            {
+                //checking if there is at least one inactive bullet or creating another
+                if (activeBullets.Count > 0)
                 {
                     activeBullet = activeBullets.Dequeue();
-                    activeBullet.transform.position = tipOfGun.position;
-                    activeBullet.transform.rotation = tipOfGun.rotation * rotate180;
+                    activeBullet.transform.position = tipTransform.position;
+                    activeBullet.transform.rotation = tipTransform.rotation * rotate180;
                     activeBullet.SetActive(true);
                 }
                 else
                 {
-                    activeBullet = Instantiate(bullet, tipOfGun.position, tipOfGun.rotation * rotate180);
+                    activeBullet = Instantiate(bullet, tipTransform.position, tipTransform.rotation * rotate180);
                     activeBullet.gameObject.GetComponent<BulletMovement>()?.setActionAddBullet(addBullet);
                 }
             }
@@ -53,13 +84,13 @@ public class GunFiringController : MonoBehaviour
                 if (activeBullets.Count > 0)
                 {
                     activeBullet = activeBullets.Dequeue();
-                    activeBullet.transform.position = tipOfGun.position;
-                    activeBullet.transform.rotation = tipOfGun.rotation;
+                    activeBullet.transform.position = tipTransform.position;
+                    activeBullet.transform.rotation = tipTransform.rotation;
                     activeBullet.SetActive(true);
                 }
                 else
                 {
-                    activeBullet = Instantiate(bullet, tipOfGun.position, tipOfGun.rotation);
+                    activeBullet = Instantiate(bullet, tipTransform.position, tipTransform.rotation);
                     activeBullet.gameObject.GetComponent<BulletMovement>()?.setActionAddBullet(addBullet);
                 }
             }
@@ -71,5 +102,11 @@ public class GunFiringController : MonoBehaviour
         canFire = false;
         yield return new WaitForSeconds(firingDelay);
         canFire = true;
+    }
+
+    private IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(reloadTime);
+        activeMagazine = magazineSize;
     }
 }
