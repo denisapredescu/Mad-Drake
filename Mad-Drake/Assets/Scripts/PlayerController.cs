db85d8fa37b40a86e04ff28eeec2d68c9ce26506
Rares Popa <rares_teodor@yahoo.ro>
2022-12-04 17:16:09 +0200

    Animation improvments for player and utility class for generation level

--- Mad-Drake/Assets/Scripts/PlayerController.cs
+++ Mad-Drake/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,13 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 
 public class PlayerController : MonoBehaviour
 {
     private new Transform transform;
     private Rigidbody2D rb2;
     private TrailRenderer trailRenderer;
+    [SerializeField]
+    private Animator animator;
 
     private float xMovement = 0.0f, yMovement = 0.0f;
     public float speed = 6.0f;
@@ -19,6 +17,7 @@ public class PlayerController : MonoBehaviour
     public float breakDash = 4.0f;
     private bool dashing = false;
     private bool startDash = false;
+    private bool run = false;
 
     private Vector3 mousePos;
     private new Camera camera;
@@ -29,10 +28,10 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
-        transform = this.gameObject.transform;
-        rb2 = this.gameObject.GetComponent<Rigidbody2D>();
-        trailRenderer = this.gameObject.GetComponent<TrailRenderer>();
-        camera = Camera.main;  
+        transform = gameObject.transform;
+        rb2 = gameObject.GetComponent<Rigidbody2D>();
+        trailRenderer = gameObject.GetComponent<TrailRenderer>();
+        camera = Camera.main;
     }
 
     private void Update() 
@@ -44,14 +43,25 @@ public class PlayerController : MonoBehaviour
 
         if (!dashing)
         {
-            //moving
-            transform.Translate(new Vector3(xMovement, yMovement, 0) * Time.deltaTime * speed);
+            //animate running when the player is not dashing
+            if (new Vector2(xMovement, yMovement).magnitude > 0.1f)
+                animator.SetBool("run", true);
+            else
+                animator.SetBool("run", false);
+
+            //start moving
+            run = true;
             
             //dashing
             if(Input.GetKeyDown(KeyCode.Space))
             {
                 startDash = true;
             }
+        }
+        else
+        {
+            run = false;
+            animator.SetBool("run", false);
         }
 
         RotatePlayer();
@@ -60,6 +70,10 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //moving
+        if (run)
+            rb2.MovePosition(transform.position + speed * Time.fixedDeltaTime * new Vector3(xMovement, yMovement, 0));
+
         //start dashing
         if(startDash && rb2 != null)
         {
@@ -78,7 +92,7 @@ public class PlayerController : MonoBehaviour
         }
         else if(dashing)
         {
-            rb2.velocity = rb2.velocity * changeDashForce;
+            rb2.velocity *= changeDashForce;
         }
     }
 
@@ -90,10 +104,6 @@ public class PlayerController : MonoBehaviour
             PlayerHealthController.AddGold();
         }
     }
-    void OnCollisionEnter2D(Collision2D col)
-{
-	Debug.Log("OnCollisionEnter2D");
-}
     
     public void RotatePlayer()
     {
