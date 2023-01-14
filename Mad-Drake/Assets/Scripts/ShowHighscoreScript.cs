6a8c73495c0d5ded6c001e0215ff8fe97e46726b
denisapredescu <denissapredescu@yahoo.com>
2023-01-14 23:03:56 +0200

    add final form of highcore

--- Mad-Drake/Assets/Scripts/ShowHighscoreScript.cs
+++ Mad-Drake/Assets/Scripts/ShowHighscoreScript.cs
@@ -27,17 +27,60 @@ public class ShowHighscoreScript : MonoBehaviour
         var highscore = JsonUtility.FromJson<Highscore>(pullData);
 
 
-        // the highscore list need to gave 5 items 
-        name1.text = highscore.players[0].name;
-        score1.text = highscore.players[0].score.ToString();
-        name2.text = highscore.players[1].name;
-        score2.text = highscore.players[1].score.ToString();
-        name3.text = highscore.players[2].name;
-        score3.text = highscore.players[2].score.ToString();
-        name4.text = highscore.players[3].name;
-        score4.text = highscore.players[3].score.ToString();
-        name5.text = highscore.players[4].name;
-        score5.text = highscore.players[4].score.ToString();
+        if (highscore.players.Count >= 1)
+        {
+            name1.text = highscore.players[0].name;
+            score1.text = highscore.players[0].score.ToString();
+        }
+        else
+        {
+            name1.text = "";
+            score1.text = "";
+        }
+
+        if (highscore.players.Count >= 2)
+        {
+            name2.text = highscore.players[1].name;
+            score2.text = highscore.players[1].score.ToString();
+        }
+        else
+        {
+            name2.text = "";
+            score2.text = "";
+        }
+
+        if (highscore.players.Count >= 3)
+        {
+            name3.text = highscore.players[2].name;
+            score3.text = highscore.players[2].score.ToString();
+        }
+        else
+        {
+            name3.text = "";
+            score3.text = "";
+        }
+
+        if (highscore.players.Count >= 4)
+        {
+            name4.text = highscore.players[3].name;
+            score4.text = highscore.players[3].score.ToString();
+        }
+        else
+        {
+            name4.text = "";
+            score4.text = "";
+        }
+
+        if (highscore.players.Count == 5)
+        {
+            name5.text = highscore.players[4].name;
+            score5.text = highscore.players[4].score.ToString();
+        }
+        else
+        {
+            name5.text = "";
+            score5.text = "";
+        }
     }
 }
 
