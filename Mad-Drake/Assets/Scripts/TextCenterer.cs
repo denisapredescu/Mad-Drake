d905a72ffcaf422c7e085d6a015dac425f141aa6
Amar Kayed <amar.kayed18@gmail.com>
2023-01-15 17:24:44 +0200

    Center Gun Score To Gun Bar

--- Mad-Drake/Assets/Scripts/TextCenterer.cs
+++ Mad-Drake/Assets/Scripts/TextCenterer.cs
@@ -1,27 +1,19 @@
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TextCenterer : MonoBehaviour
 {
     void Start()
     {
-        // Find the "Health Score" GameObject
-        Transform healthScore = transform.Find("Health Score");
+        TextMeshProUGUI childTextComponent = GetComponentInChildren<TextMeshProUGUI>();
+        Transform score = childTextComponent.transform;
+        RectTransform textRect = score.GetComponent<RectTransform>();
 
-        // Get the Text component of the "Health Score" GameObject
-        Text text = healthScore.GetComponent<Text>();
-
-        // Get the RectTransform component of the "Health Score" GameObject
-        RectTransform textRect = healthScore.GetComponent<RectTransform>();
-
-        // Set the anchor of the text to the center of the parent
         textRect.anchorMin = new Vector2(0.5f, 0.5f);
         textRect.anchorMax = new Vector2(0.5f, 0.5f);
 
-        // Set the pivot of the text to the center
         textRect.pivot = new Vector2(0.5f, 0.5f);
 
-        // Set the position of the text to the center of the parent
         textRect.localPosition = textRect.rect.center;
     }
 }
